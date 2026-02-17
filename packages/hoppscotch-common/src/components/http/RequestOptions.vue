0ddcdb515bd22683fc2f29266bbaca021b1d9554
dyoganan_ford <dyoganan@ford.com>
2026-02-17 21:16:29 +0530

    Revert cookie management commits

--- packages/hoppscotch-common/src/components/http/RequestOptions.vue
+++ packages/hoppscotch-common/src/components/http/RequestOptions.vue
@@ -50,14 +50,6 @@
       />
     </HoppSmartTab>
     <HoppSmartTab
-      v-if="properties?.includes('cookies') ?? true"
-      :id="'cookies'"
-      :label="`${t('tab.cookies')}`"
-      :info="`${newActiveCookiesCount}`"
-    >
-      <HttpCookies :url="request.endpoint" />
-    </HoppSmartTab>
-    <HoppSmartTab
       v-if="showPreRequestScriptTab"
       :id="'preRequestScript'"
       :label="`${t('tab.pre_request_script')}`"
@@ -113,19 +105,16 @@ import {
 } from "@hoppscotch/data"
 import { useVModel } from "@vueuse/core"
 import { computed } from "vue"
-import { useService } from "dioc/vue"
 
 import { defineActionHandler } from "~/helpers/actions"
 import { HoppInheritedProperty } from "~/helpers/types/HoppInheritedProperties"
 import { AggregateEnvironment } from "~/newstore/environments"
-import { CookieJarService } from "~/services/cookie-jar.service"
 
 const _VALID_OPTION_TABS = [
   "params",
   "bodyParams",
   "headers",
   "authorization",
-  "cookies",
   "preRequestScript",
   "tests",
   "requestVariables",
@@ -197,18 +186,6 @@ const newActiveRequestVariablesCount = computed(() => {
 
 const isBodyFilled = computed(() => {
   return Boolean(request.value.body.body && request.value.body.body.length > 0)
-})
-
-const newActiveCookiesCount = computed(() => {
-  if (!request.value.endpoint) return null
-
-  try {
-    const cookieJarService = useService(CookieJarService)
-    const cookies = cookieJarService.getCookiesForURL(request.value.endpoint)
-    return cookies.length > 0 ? cookies.length : null
-  } catch {
-    return null
-  }
 })
 
 defineActionHandler("request.open-tab", ({ tab }) => {
--- packages/hoppscotch-common/src/helpers/kernel/rest/response.ts
+++ packages/hoppscotch-common/src/helpers/kernel/rest/response.ts
@@ -4,8 +4,6 @@ import {
   HoppRESTResponseHeader,
   HoppRESTSuccessResponse,
 } from "~/helpers/types/HoppRESTResponse"
-import { getService } from "~/modules/dioc"
-import { CookieJarService } from "~/services/cookie-jar.service"
 
 export type HoppRESTTransformError = {
   type: "fail"
@@ -86,32 +84,9 @@ export const RESTResponse = {
       }
     }
 
-    // Process headers to handle Set-Cookie splitting
-    const headers = processHeaders(response.headers)
-
-    // Extract Set-Cookie headers and store them in the cookie jar
-    const setCookieHeaders = headers
-      .filter(header => header.key.toLowerCase() === 'set-cookie')
-      .map(header => header.value)
-
-    console.log('[Cookie Debug] Set-Cookie headers found:', setCookieHeaders.length, setCookieHeaders)
-
-    if (setCookieHeaders.length > 0) {
-      try {
-        // Get CookieJarService instance and extract cookies
-        const cookieJarService = getService(CookieJarService)
-        console.log('[Cookie Debug] Extracting cookies for URL:', originalRequest.endpoint)
-        cookieJarService.extractCookiesFromResponse(setCookieHeaders, originalRequest.endpoint)
-        console.log('[Cookie Debug] Cookies extracted successfully')
-      } catch (error) {
-        console.warn("Failed to extract cookies from response:", error)
-        // Don't fail the response if cookie extraction fails
-      }
-    }
-
     return {
       type: "success",
-      headers,
+      headers: processHeaders(response.headers),
       body: response.body.body.buffer,
       statusCode: response.status,
       statusText: response.statusText ?? "",
